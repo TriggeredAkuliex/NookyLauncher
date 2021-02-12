f5952a9a4952c824c8e913dc0781180b4daf912d
Manthe_Akuliex <75134933+TriggeredAkuliex@users.noreply.github.com>
2021-02-12 17:40:46 +0100

    Mise A Jour v1.0.2

--- build.js
+++ build.js
@@ -18,10 +18,10 @@ function getCurrentPlatform(){
 builder.build({
     targets: (process.argv[2] != null && Platform[process.argv[2]] != null ? Platform[process.argv[2]] : getCurrentPlatform()).createTarget(),
     config: {
-        appId: 'helioslauncher',
-        productName: 'Helios Launcher',
+        appId: 'nookylauncher',
+        productName: 'Nooky Launcher',
         artifactName: '${productName}-setup-${version}.${ext}',
-        copyright: 'Copyright © 2018-2020 Daniel Scalzi',
+        copyright: 'Copyright © 2018-2020 Kenzo Guerre',
         directories: {
             buildResources: 'build',
             output: 'dist'
@@ -46,10 +46,10 @@ builder.build({
         },
         linux: {
             target: 'AppImage',
-            maintainer: 'Daniel Scalzi',
-            vendor: 'Daniel Scalzi',
-            synopsis: 'Modded Minecraft Launcher',
-            description: 'Custom launcher which allows users to join modded servers. All mods, configurations, and updates are handled automatically.',
+            maintainer: 'Kenzo Guerre',
+            vendor: 'Kenzo Guerre',
+            synopsis: 'Moddé Launcher Minecraft',
+            description: 'Lanceur personnalisé qui permet aux utilisateurs de rejoindre des serveurs modifiés. Tous les mods, configurations et mises à jour sont gérés automatiquement.',
             category: 'Game'
         },
         compression: 'maximum',
@@ -62,7 +62,7 @@ builder.build({
         asar: true
     }
 }).then(() => {
-    console.log('Build complete!')
+    console.log('Construction Compléte!')
 }).catch(err => {
     console.error('Error during build!', err)
 })
